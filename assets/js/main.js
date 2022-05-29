1fa94161649dc5aed6cb4102e5d3a2c987e8e234
Bagnarola Roy <broyettore@gmail.com>
2022-05-29 17:45:58 +0200

    services, work, contact, footer sections added

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,6 @@ const header = document.querySelector("header");
 window.addEventListener ("scroll", function() {
     header.classList.toggle ("sticky", window.scrollY > 0);
 });
+
+       
+       
